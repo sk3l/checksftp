92260067f2ea6db3795859f01e3becba415896a1
sk3l <mike@skelton.onl>
2017-10-11 07:35:01 -0400

    Implement continuous check timer.

--- src/checksftp/static/checksftp.js
+++ src/checksftp/static/checksftp.js
@@ -1,3 +1,6 @@
+let checktype = "simple";
+let timer = null;
+
 $(function(){
 
      $(".dropdown-menu").on('click', 'li a', function(){
@@ -12,17 +15,32 @@ $(function(){
            });
 
       $("input[name='optionsRadios']").change(function(){
-         if ($(this).val() === 'continuous') {
+         checktype = $(this).val();
+         if (checktype === 'continuous') {
             $("#div-cont-int").show();
          }
          else {
             $("#div-cont-int").hide();
+            if (timer !== null) {
+               clearInterval(timer);
+               timer = null;
+            }
          }
       });
-     
+
 });
 
-function run_the_check(){
+function run_the_check() {
+
+   check_with_server();
+   if (checktype === 'continuous' && timer === null) {
+      interval =  $("#input-cont-int").val();
+      interval *= 1000;
+      timer = setInterval(run_the_check, interval);
+   }
+}
+
+function check_with_server(){
 
     $("#div-host-err").hide();
     $("#div-port-err").hide();
@@ -68,44 +86,48 @@ function run_the_check(){
        type    : 'POST',
        dataType: 'json',
        success : function(data){
-       alert("Success. Got the message:\n "+checkurl +"\n" + data.message)
+          alert("Success. Got the message:\n "+checkurl +"\n" + data.message)
 
-       $("#div-result-summary").text(data.msg);
+          $("#div-result-summary").text(data.msg);
 
-       $("#div-result-img").show();
-       let result = data.result;
-       if (result === 0) {
-          $("#div-result-img").html(
-            '<img alt="succcess!" src="static/images/web-check-sml.png"></img>');
-          $("#h2-outcome").text("Success");
-          $("#h2-outcome").css("color", "green");
-          $("#span-target-lbl").attr("class", "label label-success label-img");
-          $("#span-target-host").css("color", "green");
-          $("#span-target-port").css("color", "green");
-       }
-       else {
-          $("#div-result-img").html(
-             '<img alt="fail" src="static/images/web-x-sml.png"></img>');
-          $("#h2-outcome").text("Failure");
-          $("#h2-outcome").css("color", "red");
-          $("#span-target-lbl").attr("class", "label label-danger label-img");
-          $("#span-target-host").css("color", "red");
-          $("#span-target-port").css("color", "red");
-       }
+          $("#div-result-img").show();
+          let result = data.result;
+          if (result === 0) {
+             $("#div-result-img").html(
+               '<img alt="succcess!" src="static/images/web-check-sml.png"></img>');
+             $("#h2-outcome").text("Success");
+             $("#h2-outcome").css("color", "green");
+             $("#span-target-lbl").attr("class", "label label-success label-img");
+             $("#span-target-host").css("color", "green");
+             $("#span-target-port").css("color", "green");
+          }
+          else {
+             $("#div-result-img").html(
+                '<img alt="fail" src="static/images/web-x-sml.png"></img>');
+             $("#h2-outcome").text("Failure");
+             $("#h2-outcome").css("color", "red");
+             $("#span-target-lbl").attr("class", "label label-danger label-img");
+             $("#span-target-host").css("color", "red");
+             $("#span-target-port").css("color", "red");
+          }
 
-       $("#div-result-text").show()
+          $("#div-result-text").show()
 
-       if (showtrace === true) {
-          $("#panel-trace-dtl").html(data.trace);
-          $("#panel-trace").show();
-       }
-       else {
-          $("#panel-trace").hide();
-       }
-       $("#h1-title").text("Check Complete");
-       $("#div-result-outcome").show();
-       $("#div-result-summary").show();
-       $("#div-result-status").hide();
+          if (showtrace === true) {
+             $("#panel-trace-dtl").html(data.trace);
+             $("#panel-trace").show();
+          }
+          else {
+             $("#panel-trace").hide();
+          }
+
+          $("#h1-title").text("Check Complete");
+          $("#div-result-outcome").show();
+          $("#div-result-summary").show();
+          $("#div-result-status").hide();
+
+
        }
     });
 }
+
